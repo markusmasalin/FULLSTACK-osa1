3abf73e0bcb5d2b1b3fb683abe99dc2e184fda0f
Markus Joakim Masalin <markus.masalin@eduvantaa.fi>
2019-03-19 23:49:49 +0200

    new version

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { prependOnceListener } from 'cluster';
 
 const Header = (props) => {
     return (
+    <div>
     <h1>
         {props.course}
     </h1>
+    </div>
     )
 }
 
 const Content = (props) => {
     return(
     <div>
-       <Part {props.part1} {props.exercises1}/>
-       <Part {props.part2} {props.exercises2}/>
-       <Part {props.part3} {props.exercises3}/>
-
-      
-        
-        
+       <Part part ={props.part1} exercises = {props.exercises1}/>
+       <Part part ={props.part2} exercises = {props.exercises2}/> 
+       <Part part ={props.part3} exercises = {props.exercises3} /> 
     </div>
     )    
 }
 
+const Part = (props) => {
+    return(
+    <div>
+    <p> 
+        {props.part} {props.exercises}
+    </p>
+    </div>
+    )
+}
+
 const Total = (props) => {
     return (
+    <div>
     <p>yhteensä 
         {props.total} tehtävää
     </p>
+    </div>
     ) 
 
 }
@@ -44,16 +55,10 @@ const App = () => {
     return (
       <div>
         <Header course={course} />
-        <Content part1={part1} exercises1={exercises1} 
-                part2={part2} exercises2={exercises2}
-                part3={part3} exercises3={exercises3}
-        
-        />
-        
-
+        <Content part1={part1} part2={part2} part3={part3} exercises1= {exercises1} exercises2={exercises2} exercises3={exercises3}/>
         <Total total={exercises1 + exercises2 + exercises3}/>
         
-        </div>
+      </div>
     )
   }
   
