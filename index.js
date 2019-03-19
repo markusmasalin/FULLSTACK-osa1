01b63a2f27f86acf1ff01bdba617208cb322926f
Markus Joakim Masalin <markus.masalin@eduvantaa.fi>
2019-03-19 22:15:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = (props) => {
+    return (
+    <h1>
+        {props.course}
+    </h1>
+    )
+}
+
+const Content = (props) => {
+    return(
+    <div>
+       <Part {props.part1} {props.exercises1}/>
+       <Part {props.part2} {props.exercises2}/>
+       <Part {props.part3} {props.exercises3}/>
+
+      
+        
+        
+    </div>
+    )    
+}
+
+const Total = (props) => {
+    return (
+    <p>yhteensä 
+        {props.total} tehtävää
+    </p>
+    ) 
+
+}
+
+const App = () => {
+    const course = 'Half Stack -sovelluskehitys'
+    const part1 = 'Reactin perusteet'
+    const exercises1 = 10
+    const part2 = 'Tiedonvälitys propseilla'
+    const exercises2 = 7
+    const part3 = 'Komponenttien tila'
+    const exercises3 = 14
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content part1={part1} exercises1={exercises1} 
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3}
+        
+        />
+        
+
+        <Total total={exercises1 + exercises2 + exercises3}/>
+        
+        </div>
+    )
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
